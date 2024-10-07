108e69726f2534a6c150498297266cc089369a32
id21ash <124149502+id21ash@users.noreply.github.com>
2024-10-07 11:19:27 +0200

    patch_inventory, patch_category and ovve_color

--- database/init.sql
+++ database/init.sql
@@ -1,15 +1,47 @@
+DROP TABLE patch_inventory;
+DROP TABLE patch;
+DROP TABLE profile;
+DROP TABLE ovve_color;
+DROP TABLE patch_category;
+
+CREATE TABLE ovve_color (
+    id SERIAL PRIMARY KEY,
+    university TEXT,
+    determinator TEXT,
+    color_name TEXT,
+    color_hex VARCHAR(6)
+);
+
 CREATE TABLE profile (
     id SERIAL PRIMARY KEY,
     username TEXT,
     password TEXT,
-    ovvename TEXT,
+    ovve_name TEXT,
     purchase_date DATE,
     inauguration_date DATE,
-    biography TEXT
+    biography TEXT,
+    color INTEGER REFERENCES ovve_color(id)
+);
+
+CREATE TABLE patch_category (
+    id SERIAL PRIMARY KEY,
+    name TEXT
 );
 
 CREATE TABLE patch (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    creator TEXT
+    creator TEXT,
+    category INTEGER REFERENCES patch_category(id)
 );
+
+CREATE TABLE patch_inventory (
+    id SERIAL PRIMARY KEY,
+    patch_id INTEGER REFERENCES patch(id),
+    profile_id INTEGER REFERENCES profile(id),
+    price INTEGER,
+    obtained_date DATE,
+    lost_date DATE,
+    obtained_from TEXT
+);
+
