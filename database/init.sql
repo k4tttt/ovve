f799e66177af8e6b83f9abd9373534c17298b00b
id21ash <124149502+id21ash@users.noreply.github.com>
2024-10-07 10:25:43 +0200

    create profile and patch

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,15 @@
--- hej lets sql
+CREATE TABLE profile (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    ovvename TEXT,
+    purchase_date DATE,
+    inauguration_date DATE,
+    biography TEXT
+);
+
+CREATE TABLE patch (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    creator TEXT
+);
