0d5c3b9b2caf87ee5aa9bc37352e0aaa359ee429
erikosimonsson <erikosimonsson@gmail.com>
2024-10-10 12:11:48 +0200

    Query stuff
    Bugfix in checkusern.sql
    Added insert query for users
    Moved query to correct folder

---
+++ database/AddDataQueries/addusers.sql
@@ -0,0 +1,5 @@
+INSERT INTO profile (username, password, ovve_name, purchase_date, inauguration_date, biography, color)
+VALUES
+    ('Erik', 'password', 'Erik', '2022-01-15', '2022-02-10', 'Loves adventure and exploration.', NULL),
+    ('Alva', 'password', 'Tyra', '2023-03-12', '2023-04-05', 'Passionate about technology.', NULL),
+    ('Tyra', 'password', 'Alva', '2024-06-22', '2024-07-14', 'Enjoys nature and the outdoors.', NULL);
--- database/checkusern.sql
+++ database/checkusern.sql
@@ -1,10 +1,12 @@
-CREATE FUNCTION check_username_uniqueness(username TEXT)
+DROP FUNCTION check_username_uniqueness CASCADE;
+
+CREATE FUNCTION check_username_uniqueness(usern TEXT)
 RETURNS BOOLEAN AS $$
 BEGIN
 	IF EXISTS (SELECT 1  FROM profile WHERE username = $1) THEN
-		RETURN FALSE
+		RETURN FALSE;
 	ELSE
-		RETURN TRUE
+		RETURN TRUE;
 	END IF;
 END;
 $$ LANGUAGE plpgsql;
