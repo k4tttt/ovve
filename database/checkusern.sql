ff3d4d2389bea0b172493f26b7cbadfefa0e47a2
erikosimonsson <erikosimonsson@gmail.com>
2024-10-10 12:12:28 +0200

    Update checkusern.sql
    Re-fixed checkusern.sql

--- database/checkusern.sql
+++ database/checkusern.sql
@@ -1,9 +1,9 @@
-DROP FUNCTION check_username_uniqueness CASCADE;
+DROP FUNCTION IF EXISTS check_username_uniqueness;
 
 CREATE FUNCTION check_username_uniqueness(usern TEXT)
 RETURNS BOOLEAN AS $$
 BEGIN
-	IF EXISTS (SELECT 1  FROM profile WHERE username = $1) THEN
+	IF EXISTS (SELECT 1 FROM profile WHERE username = $1) THEN
 		RETURN FALSE;
 	ELSE
 		RETURN TRUE;
