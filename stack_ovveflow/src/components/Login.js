3ef89dfbb4e3205657deefe4d53f44cc409dc316
k4tttt <tyra.woden@gmail.com>
2024-10-10 11:37:20 +0200

    create login component

---
+++ stack_ovveflow/src/components/Login.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+const Login = () => {
+  const [data, setData] = useState(null);
+
+  return (
+    <div className="">
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default Login;
