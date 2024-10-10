654fd82d5b6d5bc83e4a2ba71d8cc4f0d3e9d32b
id21ash <124149502+id21ash@users.noreply.github.com>
2024-10-10 10:48:42 +0200

    ovve_colors filled

---
+++ database/dummy_data.sql
@@ -0,0 +1,33 @@
+INSERT INTO ovve_color (university, determinator, color_name, color_hex)
+VALUES
+('Umeå Universitet', 'Kandidatprogrammet i biologi och geovetenskap', 'Blå', '1E66FF'),
+('Umeå Universitet', 'Kandidatprogramet i litteraturvetenskap och kreativt skrivande, samt biblioteks- och informationsvetenskap', 'Brun', 'B96427'),
+('Umeå Universitet', 'Högskoleingenjör i maskinteknik', 'Grå', '9E9E9E'),
+('Umeå Universitet', 'Högskoleingenjör i byggteknik', 'Orange', 'FFA530'),
+('Umeå Universitet', 'Högskoleingenjör i energiteknik', 'Vinröd', 'B12222'),
+('Umeå Universitet', 'Högskoleingenjör i elektronik och datorteknik/medicinsk Teknik', 'Vit', 'F3F3F3'),
+('Umeå Universitet', 'Civilingenjör i teknisk fysik', 'Svart', '191919'),
+('Umeå Universitet', 'Civilingenjör i industriell ekonomi', 'Gul', 'FED638'),
+('Umeå Universitet', 'Civilingenjör i energiteknik', 'Mörkblå', '24269B'),
+('Umeå Universitet', 'Civilingenjör i bioteknik', 'Mörkgrön', '005A09'),
+('Umeå Universitet', 'Civilingenjör i interaktion och design', 'Khaki', 'D0C491'),
+('Umeå Universitet', 'Civilingenjör i teknisk datavetenskap', 'Ljusblå', 'A8D7F0'),
+('Umeå Universitet', 'Kandidat/masterutbildningen i datavetenskap', 'Ljusblå med mörklila revär', 'A8D7F0'),
+('Umeå Universitet', 'Digital medieproduktion', 'Ljusgul', 'EFEF8F'),
+('Umeå Universitet', 'Programmet i Biomedicin', 'Mörklila', '7126E2'),
+('Umeå Universitet', 'Kandidatprogrammet i matematik', 'Mörklila med gul revär', '7126E2'),
+('Umeå Universitet', 'Biomedicinsk analytikerprogrammet', 'Ljuslila', 'C57EFF'),
+('Umeå Universitet', 'Life science', 'Ljusgrön', '8DDB78'),
+('Umeå Universitet', 'Arkitektutbildningen', 'Rosa', 'E753CE'),
+('Umeå Universitet', 'Arkeologprogrammet', 'Neongrön', 'DEFF4A'),
+('Umeå Universitet', 'Museologiprogrammet', 'Neongrön med revär', 'DEFF4A'),
+('Umeå Universitet', 'Sjuksköterske- och röntgensjuksköterskeprogrammet', 'Röd med reflex', 'EB0808'),
+('Umeå Universitet', 'Miljö- och hälsoskyddsprogrammet', 'Turkos', '4FFFE8'),
+('Umeå Universitet', 'Lärarprogrammet', 'Ljusgrå m revär', 'B6B6B6'),
+('Umeå Universitet', 'Socionom', 'Korall', 'FF9F8E'),
+('Umeå Universitet', 'Beteendevetarsektionen', 'Maroon', 'AC1A41'),
+('Umeå Universitet', 'Tandteknikerprogrammet', 'Blekrosa', 'FDC3ED'),
+('Umeå Universitet', 'Programmet i statistik och data science', 'Mörkgröna hängselbyxor', '005A09');
+
+
+SELECT * FROM ovve_color;
