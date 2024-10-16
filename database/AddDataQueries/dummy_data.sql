52e594d1192edf03cbc7987b750b404c6961c49c
id21ash <124149502+id21ash@users.noreply.github.com>
2024-10-16 15:17:19 +0200

    the best

--- database/AddDataQueries/dummy_data.sql
+++ database/AddDataQueries/dummy_data.sql
@@ -124,3 +124,10 @@ INSERT INTO patch (name, creator, category) VALUES
 SELECT patch.name as patch, patch.creator, patch_category.name as category FROM patch
 JOIN patch_category ON patch.category = patch_category.id;
 
+INSERT INTO profile (username, password, ovve_name, purchase_date, inauguration_date, biography, color, email, type)
+VALUES
+    ('Erik', 'password', 'Erik', '2022-01-15', '2022-02-10', 'Loves adventure and exploration.', 11, 'id16esn@cs.umu.se', 1),
+    ('Alva', 'password', 'Alva', '2023-03-12', '2023-04-05', 'Passionate about technology.', 11, 'id21ash@cs.umu.se', 1),
+    ('k4tt', 'password', 'TYRA', '2022-03-22', '2024-04-14', 'sliving', 11, 'id21twn@cs.umu.se', 1);
+
+SELECT * FROM profile;
