cc0d6cce2cd0da4ef35da9fcb990837efbc0462a
id21ash <124149502+id21ash@users.noreply.github.com>
2024-10-17 11:37:16 +0200

    create status

--- api/model.js
+++ api/model.js
@@ -68,7 +68,7 @@ const create_inventory = async (userData) => {
   } = userData;
 
   try {
-    // Insert user into the profile table
+    // Insert inventory into the patch_inventory table
     const res = await pool.query(
       `INSERT INTO patch_inventory (patch_id, profile_id, price, obtained_date, lost_date, obtained_from)
       VALUES ($1, $2, $3, $4, $5, $6)
@@ -84,9 +84,32 @@ const create_inventory = async (userData) => {
   }
 };
 
+const create_status = async (userData) => {
+  const {
+    TST, TET, sewn_on, placement, patch
+  } = userData;
+
+  try {
+    // Insert status into the patch_status table
+    const res = await pool.query(
+      `INSERT INTO patch_status (TST, TET, sewn_on, placement, patch)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING id;`,
+      [TST, TET, sewn_on, placement, patch]
+    );
+
+    return res.rows[0]; 
+  } catch (err) {
+    console.error('Error inserting status:', err);
+
+    throw err;
+  }
+};
+
 module.exports = {
   get_patches,
   get_profile_by_username,
   create_user,
   create_inventory,
+  create_status,
 };
