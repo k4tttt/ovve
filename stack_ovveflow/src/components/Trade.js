74592b9c2c2ac394c384adf279a065f65742f9a8
id21ash <124149502+id21ash@users.noreply.github.com>
2024-10-29 14:46:22 +0100

    started on trades

--- stack_ovveflow/src/components/Trade.js
+++ stack_ovveflow/src/components/Trade.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
+import AddTradeOffer from './AddTradeOffer';
+
 
 function Trade({ user }) {
-  return <h1>Welcome, {user.username}!</h1>
+
+  const [add_trade_offer_active, set_add_trade_offer_active] = useState(false);
+
+  return (
+    <div>
+      <h1>Welcome, {user.username}!</h1>
+      {add_trade_offer_active ? <AddTradeOffer user={user} set_add_trade_offer_active={set_add_trade_offer_active} /> : <></>}
+      <Button variant='contained' onClick={() => set_add_trade_offer_active(true)}>Börja byta</Button>
+    </div>
+  );
 }
 
 export default Trade
