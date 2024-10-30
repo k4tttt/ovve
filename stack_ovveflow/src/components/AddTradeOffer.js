2554fce5586db9e1b3035f79c1bf582911be1959
id21ash <124149502+id21ash@users.noreply.github.com>
2024-10-30 16:16:52 +0100

    half assed component

--- stack_ovveflow/src/components/AddTradeOffer.js
+++ stack_ovveflow/src/components/AddTradeOffer.js
@@ -5,6 +5,8 @@ const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
   const [users, set_users] = useState([]);
   const [selected_user, set_selected_user] = useState(null);
   const [tradable_patches, set_tradable_patches] = useState([]);
+  const [selected_recieving_patches, set_selected_recieving_patches] = useState([{ patch_name: '', patch_inventory_id: null }]);
+  const [selected_giving_patches, set_selected_giving_patches] = useState([{ patch_name: '', patch_inventory_id: null }]);
 
   useEffect(() => {
     fetch(`http://localhost:3001/get-users`)
@@ -53,79 +55,138 @@ const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
       <form className='add_patch'>
         <h3>Börja byta</h3>
 
-        <div style={{ display: 'flex' }}>
-          <Autocomplete
-            options={users}
-            getOptionLabel={(option) => option.username}
-            value={selected_user}
-            onChange={(event, new_value) => {
-              if (new_value) {
-                set_selected_user(new_value);
-              } else {
-                // Reset state when the input is cleared
-                set_selected_user(null);
-              }
-            }}
-            renderOption={(props, option) => {
-              const { key, ...otherProps } = props; // Destructure `key` from `props`
-              return (
-                <li key={key} {...otherProps}>
-                  {option.username}
-                </li>
-              );
-            }}
-            filterOptions={(options, { inputValue }) => {
-              return options.filter(option => {
-                const lowerInputValue = inputValue.toLowerCase();
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <Autocomplete
+              options={users}
+              getOptionLabel={(option) => option.username}
+              value={selected_user}
+              onChange={(event, new_value) => set_selected_user(new_value)}
+              renderOption={(props, option) => {
+                const { key, ...otherProps } = props; // Destructure `key` from `props`
                 return (
-                  option.username.toLowerCase().includes(lowerInputValue)
+                  <li key={key} {...otherProps}>
+                    {option.username}
+                  </li>
                 );
-              });
-            }}
-            renderInput={(params) => (
-              <TextField {...params} label="Användare" variant="outlined" required />
-            )}
-            sx={{ margin: '8px', width: 300 }}
-          />
+              }}
+              filterOptions={(options, { inputValue }) => {
+                return options.filter(option => {
+                  const lowerInputValue = inputValue.toLowerCase();
+                  return (
+                    option.username.toLowerCase().includes(lowerInputValue)
+                  );
+                });
+              }}
+              renderInput={(params) => (
+                <TextField {...params} label="Användare" variant="outlined" required />
+              )}
+              sx={{ margin: '8px', width: 300 }}
+            />
 
-          <Autocomplete
-            disabled={tradable_patches.length === 0}
-            options={tradable_patches}
-            getOptionLabel={(option) => option.patch_name}
-            value={(option) => option.patch_inventory_id}
-            onChange={(event, new_value) => {
-              if (new_value) {
-                set_selected_user(new_value);
-              } else {
-                // Reset state when the input is cleared
-                set_selected_user(null);
-              }
-            }}
-            renderOption={(props, option) => {
-              const { key, ...otherProps } = props; // Destructure `key` from `props`
-              return (
-                <li key={key} {...otherProps}>
-                  {option.patch_name}
-                </li>
-              );
-            }}
-            filterOptions={(options, { inputValue }) => {
-              return options.filter(option => {
-                const lowerInputValue = inputValue.toLowerCase();
+            {selected_giving_patches.map((patch, index) => (
+              <Autocomplete
+                key={index}
+                disabled={tradable_patches.length === 0}
+                options={tradable_patches}
+                getOptionLabel={(option) => option.patch_name}
+                value={selected_giving_patches[index]}
+                onChange={(event, new_value) => set_selected_giving_patches((prev) => {
+                  const updated_patches = [...prev];
+                  updated_patches[index] = new_value;
+                  return updated_patches;
+                })}
+                renderOption={(props, option) => {
+                  const { key, ...otherProps } = props; // Destructure `key` from `props`
+                  return (
+                    <li key={key} {...otherProps}>
+                      {option.patch_name}
+                    </li>
+                  );
+                }}
+                filterOptions={(options, { inputValue }) => {
+                  return options.filter(option => {
+                    const lowerInputValue = inputValue.toLowerCase();
+                    return (
+                      option.patch_name.toLowerCase().includes(lowerInputValue)
+                    );
+                  });
+                }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Märke" variant="outlined" required />
+                )}
+                sx={{ margin: '8px', width: 300 }}
+              />
+            ))}
+            <Button variant='contained' sx={{ margin: '8px' }} onClick={() => set_selected_giving_patches((prev_patches) => [...prev_patches, { patch_name: '', patch_inventory_id: null }])}>En till från mig</Button>
+          </div>
+
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <Autocomplete
+              options={users}
+              getOptionLabel={(option) => option.username}
+              value={selected_user}
+              onChange={(event, new_value) => set_selected_user(new_value)}
+              renderOption={(props, option) => {
+                const { key, ...otherProps } = props; // Destructure `key` from `props`
                 return (
-                  option.patch_name.toLowerCase().includes(lowerInputValue)
+                  <li key={key} {...otherProps}>
+                    {option.username}
+                  </li>
                 );
-              });
-            }}
-            renderInput={(params) => (
-              <TextField {...params} label="Märke" variant="outlined" required />
-            )}
-            sx={{ margin: '8px', width: 300 }}
-          />
+              }}
+              filterOptions={(options, { inputValue }) => {
+                return options.filter(option => {
+                  const lowerInputValue = inputValue.toLowerCase();
+                  return (
+                    option.username.toLowerCase().includes(lowerInputValue)
+                  );
+                });
+              }}
+              renderInput={(params) => (
+                <TextField {...params} label="Användare" variant="outlined" required />
+              )}
+              sx={{ margin: '8px', width: 300 }}
+            />
 
+            {selected_recieving_patches.map((patch, index) => (
+              <Autocomplete
+                key={index}
+                disabled={tradable_patches.length === 0}
+                options={tradable_patches}
+                getOptionLabel={(option) => option.patch_name}
+                value={selected_recieving_patches[index]}
+                onChange={(event, new_value) => set_selected_recieving_patches((prev) => {
+                  const updated_patches = [...prev];
+                  updated_patches[index] = new_value;
+                  return updated_patches;
+                })}
+                renderOption={(props, option) => {
+                  const { key, ...otherProps } = props; // Destructure `key` from `props`
+                  return (
+                    <li key={key} {...otherProps}>
+                      {option.patch_name}
+                    </li>
+                  );
+                }}
+                filterOptions={(options, { inputValue }) => {
+                  return options.filter(option => {
+                    const lowerInputValue = inputValue.toLowerCase();
+                    return (
+                      option.patch_name.toLowerCase().includes(lowerInputValue)
+                    );
+                  });
+                }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Märke" variant="outlined" required />
+                )}
+                sx={{ margin: '8px', width: 300 }}
+              />
+            ))}
+            <Button variant='contained' sx={{ margin: '8px' }} onClick={() => set_selected_recieving_patches((prev_patches) => [...prev_patches, { patch_name: '', patch_inventory_id: null }])}>En till till mig</Button>
+          </div>
         </div>
-
-        <Button variant='contained' sx={{ margin: '8px' }} onClick={handle_submit}>Lägg till på min ovve</Button>
+        <Button variant='contained' sx={{ margin: '8px' }} onClick={handle_submit}>Skicka erbjudande</Button>
       </form>
     </div>
   );
