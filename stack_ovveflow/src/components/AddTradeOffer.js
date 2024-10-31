64a38fbe180b24f71fa78ce704ebf8f8dbb16876
id21ash <124149502+id21ash@users.noreply.github.com>
2024-10-31 11:43:11 +0100

    api connected

--- stack_ovveflow/src/components/AddTradeOffer.js
+++ stack_ovveflow/src/components/AddTradeOffer.js
@@ -1,12 +1,30 @@
 import { useEffect, useState } from 'react';
-import { Autocomplete, Button, TextField, Checkbox, FormControlLabel, MenuItem } from '@mui/material';
+import { Autocomplete, Button, TextField } from '@mui/material';
 
 const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
   const [users, set_users] = useState([]);
   const [selected_user, set_selected_user] = useState(null);
   const [tradable_patches, set_tradable_patches] = useState([]);
-  const [selected_recieving_patches, set_selected_recieving_patches] = useState([{ patch_name: '', patch_inventory_id: null }]);
+  const [logged_in_user_patches, set_logged_in_user_patches] = useState([]);
+  const [selected_receiving_patches, set_selected_receiving_patches] = useState([{ patch_name: '', patch_inventory_id: null }]);
   const [selected_giving_patches, set_selected_giving_patches] = useState([{ patch_name: '', patch_inventory_id: null }]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/get-tradable-patches-for-profile/${user.id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.result);
+        set_logged_in_user_patches(data.result);
+      })
+      .catch((error) => {
+        console.log("ERROR when fetching tradable patches: " + error);
+      });
+  }, []);
 
   useEffect(() => {
     fetch(`http://localhost:3001/get-users`)
@@ -25,8 +43,9 @@ const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
   }, []);
 
   useEffect(() => {
+    set_selected_receiving_patches([{ patch_name: '', patch_inventory_id: null }]);
     if (selected_user) {
-      fetch(`http://localhost:3001/get-tradable-patches-for-profile/3`)
+      fetch(`http://localhost:3001/get-tradable-patches-for-profile/${selected_user.id}`)
         .then((response) => {
           if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -46,7 +65,32 @@ const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
 
   const handle_submit = async (e) => {
     e.preventDefault();
-    console.log("time to trade!");
+    try {
+      const request_body = {
+        sending_profile_id: user.id,
+        receiving_profile_id: selected_user.id,
+        sending_profile_patch_ids: selected_giving_patches
+          .map(patch => patch.patch_inventory_id)
+          .filter(id => id !== null),
+        receiving_profile_patch_ids: selected_receiving_patches
+          .map(patch => patch.patch_inventory_id)
+          .filter(id => id !== null),
+      };
+
+      const response = await fetch('http://localhost:3001/create-trade-offer', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(request_body),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create trade offer');
+      }
+    } catch (error) {
+      console.log('An error occurred:', error.message);
+    }
   };
 
   return (
@@ -57,38 +101,25 @@ const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
 
         <div style={{ display: 'flex', flexDirection: 'row' }}>
           <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <Autocomplete
-              options={users}
-              getOptionLabel={(option) => option.username}
-              value={selected_user}
-              onChange={(event, new_value) => set_selected_user(new_value)}
-              renderOption={(props, option) => {
-                const { key, ...otherProps } = props; // Destructure `key` from `props`
-                return (
-                  <li key={key} {...otherProps}>
-                    {option.username}
-                  </li>
-                );
+
+            <TextField
+              defaultValue={`${user.username} (jag)`}
+              disabled
+              sx={{
+                '& .MuiInputBase-input.Mui-disabled': {
+                  color: 'rgba(0, 0, 0, 0.87)', // Set to the same color as enabled text field
+                  WebkitTextFillColor: 'rgba(0, 0, 0, 0.87)', // Required for Safari
+                },
+                margin: '8px',
+                width: '300px'
               }}
-              filterOptions={(options, { inputValue }) => {
-                return options.filter(option => {
-                  const lowerInputValue = inputValue.toLowerCase();
-                  return (
-                    option.username.toLowerCase().includes(lowerInputValue)
-                  );
-                });
-              }}
-              renderInput={(params) => (
-                <TextField {...params} label="Användare" variant="outlined" required />
-              )}
-              sx={{ margin: '8px', width: 300 }}
             />
 
             {selected_giving_patches.map((patch, index) => (
               <Autocomplete
                 key={index}
-                disabled={tradable_patches.length === 0}
-                options={tradable_patches}
+                disabled={logged_in_user_patches.length === 0}
+                options={logged_in_user_patches}
                 getOptionLabel={(option) => option.patch_name}
                 value={selected_giving_patches[index]}
                 onChange={(event, new_value) => set_selected_giving_patches((prev) => {
@@ -113,7 +144,7 @@ const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
                   });
                 }}
                 renderInput={(params) => (
-                  <TextField {...params} label="Märke" variant="outlined" required />
+                  <TextField {...params} label="Märke" variant="outlined" required={index == 0} />  // helperText={logged_in_user_patches.length === 0 ? "Du har inga märken" : ""}
                 )}
                 sx={{ margin: '8px', width: 300 }}
               />
@@ -149,14 +180,14 @@ const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
               sx={{ margin: '8px', width: 300 }}
             />
 
-            {selected_recieving_patches.map((patch, index) => (
+            {selected_receiving_patches.map((patch, index) => (
               <Autocomplete
                 key={index}
                 disabled={tradable_patches.length === 0}
                 options={tradable_patches}
                 getOptionLabel={(option) => option.patch_name}
-                value={selected_recieving_patches[index]}
-                onChange={(event, new_value) => set_selected_recieving_patches((prev) => {
+                value={selected_receiving_patches[index]}
+                onChange={(event, new_value) => set_selected_receiving_patches((prev) => {
                   const updated_patches = [...prev];
                   updated_patches[index] = new_value;
                   return updated_patches;
@@ -178,12 +209,12 @@ const AddTradeOffer = ({ user, set_add_trade_offer_active }) => {
                   });
                 }}
                 renderInput={(params) => (
-                  <TextField {...params} label="Märke" variant="outlined" required />
+                  <TextField {...params} label="Märke" variant="outlined" required={index == 0} />
                 )}
                 sx={{ margin: '8px', width: 300 }}
               />
             ))}
-            <Button variant='contained' sx={{ margin: '8px' }} onClick={() => set_selected_recieving_patches((prev_patches) => [...prev_patches, { patch_name: '', patch_inventory_id: null }])}>En till till mig</Button>
+            <Button variant='contained' sx={{ margin: '8px' }} onClick={() => set_selected_receiving_patches((prev_patches) => [...prev_patches, { patch_name: '', patch_inventory_id: null }])}>En till till mig</Button>
           </div>
         </div>
         <Button variant='contained' sx={{ margin: '8px' }} onClick={handle_submit}>Skicka erbjudande</Button>
